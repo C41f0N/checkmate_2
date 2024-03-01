27be5eb55527ddb4c478465c16627c0def867da7
Sarim Ahmed <88588593+thenoisyninga@users.noreply.github.com>
2024-03-01 20:50:03 +0500

    wrote code to encode entire data structure into json

--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -6,9 +6,22 @@ class Task extends ChangeNotifier {
 
   Task(this.name, this.completed);
 
+  Task.fromJson(Map<String, dynamic> jsonData) {
+    name = jsonData["name"];
+    completed = jsonData["completed"];
+  }
+
   // To toggle the task completion
   void toggleCompletion() {
     completed = !completed;
     notifyListeners();
+  }
+
+  // Convert object to Json
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'completed': completed,
+    };
   }
 }
